b7cbf8387e16aaee66cbd2de067088b438e29f41
xingjiepan <xingjiepan@gmail.com>
2016-02-15 21:29:48 -0800

    Make importing GraphVisualizer conditional

--- ScientificProjectManager/__init__.py
+++ ScientificProjectManager/__init__.py
@@ -3,4 +3,7 @@ from .MetaDataManager import MetaDataManager
 #Base class of research steps
 from .ResearchStep    import ResearchStep
 #Visualize the project graph
-from .GraphVisualizer import GraphVisualizer
+try:
+  from .GraphVisualizer import GraphVisualizer
+except ImportError:
+  print 'Cannot import GraphVisualizer!\n'
